de57190126baada90cb6adf84cdb6d300fcdab18
jordanibasilio264-afk <jordanibasilio264@gmail.com>
2026-03-12 14:59:44 -0400

    Inicio

---
+++ Bancos.js
@@ -0,0 +1,186 @@
+let InfoBanco = {
+    Bancos: [
+        {
+            id: "0",
+            Banco: "Banco Banreservas",
+            Personas: [
+                {
+                    parid: "0",
+                    Cliente: "Rubén Cortés",
+                    cuenta: "21873681"
+                },
+                {
+                    parid: "1",
+                    Cliente: "Hugo Lozano",
+                    cuenta: "21271637"
+                },
+                {
+                    parid: "2",
+                    Cliente: "Samuel Campos",
+                    cuenta: "378291379"
+                },
+                {
+                    parid: "3",
+                    Cliente: "Adriana Román",
+                    cuenta: "28176381"
+                },
+                {
+                    parid: "4",
+                    Cliente: "Raquel Díaz",
+                    cuenta: "3821638"
+                },
+                {
+                    parid: "5",
+                    Cliente: "Clara Martínez",
+                    cuenta: "92348237"
+                },
+            ]
+        },
+        {
+            id: "1",
+            Banco: "Banco Popular",
+            Personas: [
+                {
+                    parid: "0",
+                    Cliente: "Hugo Lozano",
+                    cuenta: "200000"
+                },
+                {
+                    parid: "1",
+                    Cliente: "Juan Francisco Gómez",
+                    cuenta: "123987921"
+                },
+                {
+                    parid: "2",
+                    Cliente: "Manuel López",
+                    cuenta: "718263"
+                },
+                {
+                    parid: "3",
+                    Cliente: "Ana Isabel Jiménez",
+                    cuenta: "218379821"
+                },
+                {
+                    parid: "4",
+                    Cliente: "Josep Rojas",
+                    cuenta: "291739"
+                },
+                {
+                    parid: "5",
+                    Cliente: "María Dolores Morales",
+                    cuenta: "21873612"
+                }
+            ]
+        },
+        {
+            id: "2",
+            Banco: "Banco BHD León",
+            Personas: [
+                {
+                    parid: "0",
+                    Cliente: "Camilo Rodriguez",
+                    cuenta: "439082"
+                },
+                {
+                    parid: "1",
+                    Cliente: "Maria Perez",
+                    cuenta: "3897239847"
+                },
+                {
+                    parid: "2",
+                    Cliente: "Carlos Martinez",
+                    cuenta: "23497234"
+                },
+                {
+                    parid: "3",
+                    Cliente: "Manuel Garcia",
+                    cuenta: "2387429"
+                },
+                {
+                    parid: "4",
+                    Cliente: "Juan Sanchez",
+                    cuenta: "328476238"
+                },
+                {
+                    parid: "5",
+                    Cliente: "Jordani Castillo",
+                    cuenta: "32784632"
+                }
+            ]
+        },
+        {
+            id: "3",
+            Banco: "Banco Scotiabank",
+            Personas: [
+                {
+                    parid: "0",
+                    Cliente: "Julio Ramirez",
+                    cuenta: "23482634"
+                },
+                {
+                    parid: "1",
+                    Cliente: "Sofia Diaz",
+                    cuenta: "239847234"
+                },
+                {
+                    parid: "2",
+                    Cliente: "Camila Gonzalez",
+                    cuenta: "4239847294"
+                },
+                {
+                    parid: "3",
+                    Cliente: "Pedro Reyes",
+                    cuenta: "238429"
+                },
+                {
+                    parid: "4",
+                    Cliente: "Juan Hernadez",
+                    cuenta: "239842398"
+                },
+                {
+                    parid: "5",
+                    Cliente: "Daniel Jimenez",
+                    cuenta: "2394723"
+                }
+            ]
+        },
+        {
+            id: "4",
+            Banco: "Banco Santa Cruz",
+            Personas: [
+                {
+                    parid: "0",
+                    Cliente: "Camilo Rodriguez",
+                    cuenta: "32487239"
+                },
+                {
+                    parid: "1",
+                    Cliente: "Maria Perez",
+                    cuenta: "3284293"
+                },
+                {
+                    parid: "2",
+                    Cliente: "Carlos Martinez",
+                    cuenta: "2387429"
+                },
+                {
+                    parid: "3",
+                    Cliente: "Manuel Garcia",
+                    cuenta: "23987429"
+                },
+                {
+                    parid: "4",
+                    Cliente: "Juan Sanchez",
+                    cuenta: "234982734"
+                },
+                {
+                    parid: "5",
+                    Cliente: "Jordani Castillo",
+                    cuenta: "23948723"
+                }
+            ]
+        }
+    ]
+}
+
+module.exports.InfoBanco = InfoBanco
---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const { InfoBanco } = require('./Bancos');
+
+app.get('/banco', (req,res) => {
+    const resultado = InfoBanco.Bancos
+    res.send(resultado)
+})
+
+app.get('/banco/:id', (req,res) => {
+    const id = req.params.id;
+    const resultado = InfoBanco.Bancos.filter(Bancos => Bancos.id === id)
+    if (!resultado) {
+        return res.status(404).send(`Nos se encontro banco ${id}`)
+    }
+    res.send(resultado)
+})
+
+app.get('/banco/:id/personas', (req,res) => {
+    const id = req.params.id;
+    const resultado = InfoBanco.Bancos.filter(Bancos => Bancos.id === id)
+    if (!resultado) {
+        return res.status(404).send(`Nos se encontro banco ${id}`)
+    }
+    res.send(resultado)
+})
+
+app.get('/banco/:id/personas/:parid', (req,res) => {
+    const id = req.params.id;
+    const parid = req.params.parid;
+    const resultado = InfoBanco.Bancos.find(Bancos => Bancos.id === id)
+    const resultado1 = resultado.Personas.find(p => p.parid === parid)
+    if (!resultado1) {
+        return res.status(404).send(`Nos se encontro banco ${id} ni se encontro persona ${parid}`)
+    }
+    res.send(resultado1)
+})
+
+app.post('/banco', (req,res) => {
+    const nuevo = req.body;
+    const resultado = InfoBanco.Bancos.push(nuevo)
+    res.send(resultado)
+})
+
+app.post('/banco/:id/personas', (req,res) => {
+    const nuevo = req.body;
+    const id = req.params.id;
+    const nuevo1 = {
+        parid: "6",
+        Cliente: "Juan Miguel",
+        cuenta: "9237497"
+    };
+    Object.assign(InfoBanco.Bancos[id],nuevo1,nuevo);
+    res.send('Espero que funcione');
+})
+
+app.put('/banco/:id/personas/:parid', (req,res) => {
+    const id = req.params.id;
+    const parid = req.params.parid;
+    const nuevoid = req.body;
+    const resultado = InfoBanco.Bancos.find(B => B.id === id)
+    const resultado1 = resultado.Personas.find(p => p.parid == parid)
+    Object.assign(resultado1,nuevoid)
+    res.send("Datos modificados.")
+})
+
+app.delete('/banco/:id/personas/:parid', (req,res) => {
+    const id = req.params.id;
+    const parid = req.params.parid;
+    const resultado = InfoBanco.Bancos.find(B => B.id === id)
+    const resultado1 = resultado.Personas.filter(p => p.parid == parid)
+    const index = resultado.Personas.findIndex(p => p.parid == parid);
+    resultado.Personas.splice(index, 1)
+    res.send('Datos eliminados')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor abrierto ${port}...`)
+})
